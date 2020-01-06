35ea4e4795502542dfaa79d8b56184e74f4ec335
RingJung <khopark123@naver.com>
2020-01-06 18:06:59 +0900

    Result Activity

--- app/src/main/java/com/example/push_ups/Main2_Act.kt
+++ app/src/main/java/com/example/push_ups/Main2_Act.kt
@@ -1,11 +1,8 @@
 package com.example.push_ups
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import io.realm.Realm
-import io.realm.RealmObject
-import io.realm.annotations.PrimaryKey
+
 
 class Main2_Act : AppCompatActivity() {
 
@@ -13,35 +10,7 @@ class Main2_Act : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
-        Realm.init(this)
-        Realm.getDefaultInstance().use { realm ->
-            realm.executeTransaction {
 
-                val member = Member()
-                member.id = 1
-                member.name = "Tom"
-                member.age = 25
-
-                realm.copyToRealm(member)
-            }
-        }
-
-        Realm.getDefaultInstance().use { realm ->
-
-            realm.where(Member::class.java).findAll().forEach {
-                Log.d("db", "${it.id}")
-                Log.d("db", "${it.name}")
-                Log.d("db", "${it.age}")
-            }
-        }
     }
 }
 
-open class Member: RealmObject() {
-
-    @PrimaryKey
-    open var id: Int = 0
-
-    open var name: String? = null
-    open var age: Int = 0
-}
