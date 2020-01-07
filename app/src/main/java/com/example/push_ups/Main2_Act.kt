3d56b62098f291bcb26a46983a113588a7fe157a
Buffer_Park <ringjung2864@gmail.com>
2020-01-07 10:53:05 +0900

    add Realm

--- app/src/main/java/com/example/push_ups/Main2_Act.kt
+++ app/src/main/java/com/example/push_ups/Main2_Act.kt
@@ -2,6 +2,12 @@ package com.example.push_ups
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+import io.realm.Realm
+import kotlinx.android.synthetic.main.activity_main2.*
+import java.util.ArrayList
 
 
 class Main2_Act : AppCompatActivity() {
@@ -9,8 +15,34 @@ class Main2_Act : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        setBarChart()
+        Realm.init(this)
+    }
 
+    private fun setBarChart() {
+        var num = 0
+        Realm.getDefaultInstance().use { realm ->
+            realm.where(Member::class.java).findAll().forEach {
+                val entries = ArrayList<BarEntry>()
+                entries.add(BarEntry(it.cnt.toFloat(), num++))
 
+                val barDataSet = BarDataSet(entries, "Cells")
+
+                val labels = ArrayList<String>()
+                labels.add(it.id)
+
+                val data = BarData(labels, barDataSet)
+                barChart.data = data // set the data and list of lables into chart
+
+                barChart.setDescription("Set Bar Chart Description")  // set the description
+
+                //barDataSet.setColors(ColorTemplate.COLORFUL_COLORS)
+                barDataSet.color = resources.getColor(R.color.colorAccent)
+
+                barChart.animateY(5000)
+
+            }
+        }
     }
 }
 
--- app/src/main/java/com/example/push_ups/MainActivity.kt
+++ app/src/main/java/com/example/push_ups/MainActivity.kt
@@ -6,19 +6,26 @@ import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.startActivity
-
+import io.realm.Realm
+import io.realm.RealmObject
+import io.realm.annotations.PrimaryKey
+import java.time.LocalDateTime
 
 class MainActivity : AppCompatActivity() {
 
-    var listener : SensorListener? = null
+    var listener: SensorListener? = null
     var manager: SensorManager? = null
     var playing = false
 
     var push_Count = 0
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,13 +34,12 @@ class MainActivity : AppCompatActivity() {
         manager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
         listener = SensorListener()
 
-        Start_Button.setOnClickListener{ view ->
-            if(playing == true){
+        Start_Button.setOnClickListener { view ->
+            if (playing == true) {
                 manager?.unregisterListener(listener)
                 Start_Button.text = "계속"
                 playing = !playing
-            }
-            else {
+            } else {
                 var sensor = manager?.getDefaultSensor(Sensor.TYPE_PROXIMITY)
                 var chk = manager?.registerListener(listener, sensor, SensorManager.SENSOR_DELAY_UI)
 
@@ -44,7 +50,7 @@ class MainActivity : AppCompatActivity() {
                 playing = !playing
             }
         }
-        Reset_Button.setOnClickListener{view->
+        Reset_Button.setOnClickListener { view ->
             manager?.unregisterListener(listener)
             push_Count = 0;
             Push_Count.text = "${push_Count}"
@@ -52,7 +58,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         Push_Count.setOnClickListener { view ->
-            if(playing == true){
+            if (playing == true) {
                 listener!!.counting_button()
             }
         }
@@ -65,11 +71,11 @@ class MainActivity : AppCompatActivity() {
 
         var push_det = false
         override fun onSensorChanged(event: SensorEvent?) {
-            if(event?.sensor?.type == Sensor.TYPE_PROXIMITY){
+            if (event?.sensor?.type == Sensor.TYPE_PROXIMITY) {
                 //textView.text = "물체와의 거리: ${event?.values[0]} cm"
 
                 push_det = !push_det
-                if(push_det == true) {
+                if (push_det == true) {
                     push_Count++
                     Push_Count.text = "${push_Count}"
                 }
@@ -81,9 +87,41 @@ class MainActivity : AppCompatActivity() {
 
         }
 
-        fun counting_button(){
+        fun counting_button() {
             push_Count++
             Push_Count.text = "${push_Count}"
         }
     }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onPause() {
+        super.onPause()
+
+        val onlyDate: LocalDateTime = LocalDateTime.now()
+        Realm.init(this)
+
+        Log.d("date", onlyDate.toString())
+
+        Realm.getDefaultInstance().use { realm ->
+
+            realm.executeTransaction {
+
+                val member = Member()
+                member.id = onlyDate.toString()
+                Log.d("data_realm",member.id)
+                member.cnt = push_Count.toInt()
+                Log.d("data_realm",member.id)
+
+                realm.copyToRealm(member)
+            }
+
+        }
+    }
+}
+
+public open class Member : RealmObject() {
+
+    @PrimaryKey
+    public open var id: String = ""
+    public open var cnt: Int = 0
 }
